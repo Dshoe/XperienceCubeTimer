b6cd32da54e4c5c01f4a7038619d6cf574c3c0b0
Devin Shoemaker <devinshoe@gmail.com>
2013-10-29 19:53:59 -0500

    Generate scramble at the beginning of the main activity

--- src/com/devinshoe/xperiencecubetimer/ActivityOne.java
+++ src/com/devinshoe/xperiencecubetimer/ActivityOne.java
@@ -3,13 +3,19 @@ package com.devinshoe.xperiencecubetimer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class ActivityOne extends Activity {
+	// Initialize tvScramble
+	TextView tvScramble = (TextView) findViewById(R.id.tvScramble);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one);
+        
+        // Generate scramble and set it to tvScramble on boot
+        tvScramble.setText(Scramble.ThreeByThree());
     }
 
 
